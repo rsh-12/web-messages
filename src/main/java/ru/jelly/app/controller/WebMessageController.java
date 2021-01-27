60c7d4be820b7468316e6c676b47c90b649c246c
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-01-27 10:07:52 +0500

    Update controller

--- src/main/java/ru/jelly/app/controller/WebMessageController.java
+++ src/main/java/ru/jelly/app/controller/WebMessageController.java
@@ -4,18 +4,33 @@ package ru.jelly.app.controller;
  * Time: 8:32 PM
  * */
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import ru.jelly.app.entity.WebMessage;
+import ru.jelly.app.repository.WebMessageRepository;
 
 @Controller
 public class WebMessageController {
+
+    @Autowired
+    private WebMessageRepository repository;
+
+    @GetMapping
+    public String index(Model model) {
+        model.addAttribute("messages", repository.findAll(Sort.by("id").ascending()));
+        return "index";
+    }
 
     @MessageMapping("/message")
     @SendTo("/topic/messages")
     public WebMessage message(WebMessage message) {
         System.out.println("message = " + message);
+        repository.save(message);
         return message;
     }
 }
