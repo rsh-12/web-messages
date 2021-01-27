b187fe3fa4ec79138ff5d1136908d695751ef804
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-01-27 10:08:02 +0500

    Add native query

--- src/main/java/ru/jelly/app/repository/WebMessageRepository.java
+++ src/main/java/ru/jelly/app/repository/WebMessageRepository.java
@@ -1,7 +1,14 @@
-//package ru.jelly.app.repository;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import ru.jelly.app.entity.WebMessage;
-//
-//public interface WebMessageRepository extends JpaRepository<WebMessage, Long> {
-//}
+package ru.jelly.app.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import ru.jelly.app.entity.WebMessage;
+
+import java.util.List;
+
+public interface WebMessageRepository extends JpaRepository<WebMessage, Long> {
+
+    @Query(nativeQuery = true, value = "select * from message order by id desc offset ?1")
+    List<WebMessage> firstMessages(@Param("quantity") int quantity);
+}
