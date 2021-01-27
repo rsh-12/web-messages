746e53b5bf47a3405537fcf68e2512b1770001e9
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-01-27 10:05:32 +0500

    Enable scheduling

--- src/main/java/ru/jelly/app/SimpleChatApplication.java
+++ src/main/java/ru/jelly/app/SimpleChatApplication.java
@@ -2,8 +2,10 @@ package ru.jelly.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SimpleChatApplication {
 
     public static void main(String[] args) {
