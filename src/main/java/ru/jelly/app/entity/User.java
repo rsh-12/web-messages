506f099510ec49d8d15ab4297fbe93babc195e72
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-01-28 08:22:05 +0500

    Add user entity

---
+++ src/main/java/ru/jelly/app/entity/User.java
@@ -0,0 +1,62 @@
+package ru.jelly.app.entity;
+/*
+ * Date: 1/28/21
+ * Time: 7:38 AM
+ * */
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "t_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    public User() {
+    }
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                '}';
+    }
+}
