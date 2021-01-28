e41473057a2bb2760a6d32c1bde3a42e29e426eb
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-01-28 08:21:52 +0500

    Implement user details

---
+++ src/main/java/ru/jelly/app/security/SecurityUser.java
@@ -0,0 +1,60 @@
+package ru.jelly.app.security;
+/*
+ * Date: 1/28/21
+ * Time: 7:51 AM
+ * */
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import ru.jelly.app.entity.User;
+
+import java.util.Collection;
+import java.util.List;
+
+public class SecurityUser implements UserDetails {
+
+    private final User user;
+
+    public SecurityUser(User user) {
+        this.user = user;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(() -> "READ");
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
