5aed763a25243fc3b635a7c0ac0f185901f2f021
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-01-30 08:41:57 +0500

    Update config

--- src/main/java/ru/jelly/app/config/WebSecurityConfig.java
+++ src/main/java/ru/jelly/app/config/WebSecurityConfig.java
@@ -4,9 +4,12 @@ package ru.jelly.app.config;
  * Time: 8:05 AM
  * */
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.Profiles;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,6 +19,9 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    Environment env;
 
     @Profile("!dev")
     @Bean
@@ -33,20 +39,25 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .mvcMatchers("/login/*", "registration/*", "/resources/**", "/static/**").permitAll()
-                .and()
-                .formLogin()
-                .loginPage("/login")
-                .defaultSuccessUrl("/", true).permitAll()
-                .and()
-                .logout()
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "POST"))
-                .invalidateHttpSession(true)
-                .clearAuthentication(true)
-                .deleteCookies("JSESSIONID")
-                .logoutSuccessUrl("/login");
 
-        http.authorizeRequests().anyRequest().authenticated();
+        if (env.acceptsProfiles(Profiles.of("dev"))) {
+            http.authorizeRequests().anyRequest().permitAll();
+        } else {
+            http.authorizeRequests()
+                    .mvcMatchers("/login/*", "registration/*", "/resources/**", "/static/**").permitAll()
+                    .and()
+                    .formLogin()
+                    .loginPage("/login")
+                    .defaultSuccessUrl("/", true).permitAll()
+                    .and()
+                    .logout()
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "POST"))
+                    .invalidateHttpSession(true)
+                    .clearAuthentication(true)
+                    .deleteCookies("JSESSIONID")
+                    .logoutSuccessUrl("/login");
+
+            http.authorizeRequests().anyRequest().authenticated();
+        }
     }
 }
