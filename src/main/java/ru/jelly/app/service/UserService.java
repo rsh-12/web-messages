87b074b9455d9da4fabc852313d3e472fc7220be
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-02-06 08:19:34 +0500

    Add registerUser method

--- src/main/java/ru/jelly/app/service/UserService.java
+++ src/main/java/ru/jelly/app/service/UserService.java
@@ -1,7 +1,11 @@
 package ru.jelly.app.service;
 
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import ru.jelly.app.entity.User;
 
 public interface UserService {
     User findByUsername(String username);
+
+    String registerUser(User candidate, BindingResult bindingResult, Model model);
 }
