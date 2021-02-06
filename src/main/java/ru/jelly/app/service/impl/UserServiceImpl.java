485bb77ea43c212e76f66eb5a65a5f8b2a160b8f
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-02-06 08:19:48 +0500

    Implement registerUser method

--- src/main/java/ru/jelly/app/service/impl/UserServiceImpl.java
+++ src/main/java/ru/jelly/app/service/impl/UserServiceImpl.java
@@ -7,7 +7,10 @@ package ru.jelly.app.service.impl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import ru.jelly.app.entity.User;
 import ru.jelly.app.repository.UserRepository;
 import ru.jelly.app.service.UserService;
@@ -19,10 +22,12 @@ import java.util.function.Supplier;
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Autowired
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
     @Override
@@ -32,5 +37,41 @@ public class UserServiceImpl implements UserService {
         log.info("> get the user by username: {}", username);
         return userRepository.findUserByUsername(username)
                 .orElseThrow(supplier);
+    }
+
+    @Override
+    public String registerUser(User candidate, BindingResult bindingResult, Model model) {
+
+        boolean errors = isFieldHasErrors(candidate, bindingResult, model);
+        if (errors) {
+            return "security/registration";
+        }
+
+        encodeAndSave(candidate);
+        return "redirect:/";
+    }
+
+
+    /* Helper methods */
+    private void encodeAndSave(User candidate) {
+        candidate.setPassword(bCryptPasswordEncoder.encode(candidate.getPassword()));
+        userRepository.save(candidate);
+    }
+
+    private boolean isFieldHasErrors(User candidate, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return true;
+        }
+
+        if (!candidate.getPassword().equals(candidate.getConfirmPassword())) {
+            model.addAttribute("passwordError", "Passwords do not match");
+            return true;
+        }
+
+        if (userRepository.existsByUsername(candidate.getUsername())) {
+            model.addAttribute("usernameError", "A user with the same name already exists");
+            return true;
+        }
+        return false;
     }
 }
